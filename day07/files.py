293c4f3cc42864876b07a73fd3af15a49eecd910
Andrew Cameron <andrew@acameron.org>
2022-12-19 21:30:00 -0500

    add solution for day 7, part 2

--- day07/files.py
+++ day07/files.py
@@ -2,12 +2,16 @@
 
 from __future__ import annotations
 from io import TextIOBase
+from typing import Iterator
 
 import re
+import sys
 
 
 _digits = re.compile('^\d+$')
-_MAX_SIZE = 100_000
+_MAX_SIZE_PT1 = 100_000
+_FILESYSTEM_CAPACITY = 70000000
+_NEEDED_SPACE = 30000000
 
 
 class File:
@@ -34,6 +38,15 @@ class Directory:
             self.children = children
         else:
             self.children = []
+
+    def traverse(self) -> Iterator[File | Directory]:
+        to_visit: list[File | Directory] = [self]
+        while to_visit:
+            next_item = to_visit.pop()
+            if isinstance(next_item, Directory):
+                to_visit.extend(next_item.children)
+            yield next_item
+        return
 
     def fullname(self) -> str:
         d: Directory | None = self
@@ -95,6 +108,7 @@ def parse_cd(lines: TextIOBase, cwd: Directory | None, new_dir: str) -> Director
             assert not cwd, "tried to cd / more than once"
             Directory.to_visit.remove('/')
             return Directory('/')
+    raise ValueError(f"received nonsensical cd argument: {new_dir}")
 
 
 def parse_ls(lines: TextIOBase, cwd: Directory | None) -> None:
@@ -124,13 +138,22 @@ def parse_ls(lines: TextIOBase, cwd: Directory | None) -> None:
 
 if __name__ == '__main__':
     with open('day07/input', 'r') as f:
-        d = parse_history(f)
-        to_visit = [d]
+        root = parse_history(f)
+        if not root:
+            print("no directories found", sys.stderr)
+            sys.exit(1)
         matching_dirs = []
-        while to_visit:
-            next_dir = to_visit.pop()
-            if next_dir.size() < _MAX_SIZE:
+        for next_dir in root.traverse():
+            if not isinstance(next_dir, Directory):
+                continue
+            if next_dir.size() < _MAX_SIZE_PT1:
                 matching_dirs.append(next_dir)
-            is_dir = lambda direntry: isinstance(direntry, Directory)
-            to_visit.extend(filter(is_dir, next_dir.children))
-        print(sum(d.size() for d in matching_dirs))
+        pt1_ans = sum(d.size() for d in matching_dirs)
+        free_space = _FILESYSTEM_CAPACITY - root.size()
+        space_to_free = _NEEDED_SPACE - free_space
+        pt2_ans = min(d.size() for d in root.traverse() if
+                      isinstance(d, Directory) and
+                      d.size() >= space_to_free)
+
+        print(f"part 1: {pt1_ans}")
+        print(f"part 2: {pt2_ans}")
